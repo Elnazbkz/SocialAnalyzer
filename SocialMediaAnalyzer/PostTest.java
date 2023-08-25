98d438d32467dfbe2aba887a788d45fdcb9ce13d
Elnaz Beikzadeh <elnaz.beikzadeh@yahoo.com>
2023-08-25 16:07:35 +1000

    changes on post test

--- SocialMediaAnalyzer/PostTest.java
+++ SocialMediaAnalyzer/PostTest.java
@@ -5,11 +5,11 @@ import static org.junit.Assert.*;
 import java.time.LocalDateTime;
 
 import org.junit.Test;
-
+/// Post test case
 public class PostTest {
 
 	@Test
-	public void TestFormattedDateTime() {
+	public void TestFormattedDateTime() { // check date and time format
 		LocalDateTime datetime = LocalDateTime.of(2023,  8, 25, 15, 30);
 		Post post = new Post(1, "Test Content", "Test Author", 10, 11, datetime);
 		String expectedFormattedDateTime = "25/08/2023 15:30";
